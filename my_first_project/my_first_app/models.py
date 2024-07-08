cec2233dc24934e16dd6c9677537f7c37b39b53a
rockband_666 <rockband_666@hotmail.com>
2024-07-07 23:57:22 -0400

    comienzo de django

---
+++ my_first_project/my_first_app/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Car(models.Model):
+    title = models.TextField(max_length=250)
+    year = models.TextField(max_length=4, null=True)
+    
+    def __str__(self):
+        return f"{self.title} - {self.year}"
+    
+class Publisher(models.Model):
+    name = models.TextField(max_length=200)
+    adrress = models.TextField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.TextField(max_length=200)
+    publication_date = models.DateField()
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
